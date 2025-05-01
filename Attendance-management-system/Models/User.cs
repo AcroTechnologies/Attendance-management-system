c59dafa4a76f940975ddf0efe1f2875642c4efbc
fresher_usr <training@acrocorp.com>
2025-05-01 12:56:48 +0530

    Pull From master

--- Attendance-management-system/Models/User.cs
+++ Attendance-management-system/Models/User.cs
@@ -3,7 +3,7 @@
     public class User
     {
         public int Id { get; set; }
-        //Hello Guys this is Paras Jain
+        //Hello Guys this is Paras Jain and final push 
 
         public int UserName { get; set; }
 
